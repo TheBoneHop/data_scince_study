7ac42f7872fd08fab29a895d931b7aa6921d4710
TheBoneHop <thebonehop@gmail.com>
2023-10-10 22:41:49 +0300

    Start game_core_v3

--- project_0/game_core_v3.py
+++ project_0/game_core_v3.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+
+    # Ваш код заканчивается здесь
+
+    #return count
