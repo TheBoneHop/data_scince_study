0cdcbb2d77e637afad057a40d644c34b668243fe
TheBoneHop <thebonehop@gmail.com>
2023-10-12 12:14:55 +0300

    Reworked game_core_v3.py

--- project_0/game_core_v3.py
+++ project_0/game_core_v3.py
@@ -11,117 +11,107 @@ def game_core_v3(number: int = 1) -> int:
     Returns:
         int: Число попыток
     """
-
-def game_core_v3(number: int = 1) -> int:
-    """
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
     # Ваш код начинается здесь
-    
     count = 0
     predict = np.random.randint(1, 101)
     #Прописываем условия угадывания числа
     while number != predict:
         count += 1
-        if number < predict:
+        if number > predict:
             if predict < 20:
-                number += 1
-            elif predict >= 20 and predict <= 30:
-                if number < 10:
-                    number += 10
+                predict += 1
+            elif number >= 20 and number <= 30:
+                if predict < 10:
+                    predict += 10
                 else:
-                    number += 1
-            elif predict >= 30 and predict <= 40:
-                if number < 20:
-                    number += 20
+                    predict += 1
+            elif number >= 30 and number <= 40:
+                if predict < 20:
+                    predict += 20
                 else:
-                    number += 1
-            elif predict >= 40 and predict <= 50:
-                if number < 40:
-                    number += 30
+                    predict += 1
+            elif number >= 40 and number <= 50:
+                if predict < 40:
+                    predict += 30
                 else:
-                    number += 1
-            elif predict >= 50 and predict <= 60:
-                if number < 50:
-                    number += 10
+                    predict += 1
+            elif number >= 50 and number <= 60:
+                if predict < 50:
+                    predict += 10
                 else:
-                    number += 1
-            elif predict >= 60 and predict <= 70:
-                if number < 60:
-                    number += 10
+                    predict += 1
+            elif number >= 60 and number <= 70:
+                if predict < 60:
+                    predict += 10
                 else:
-                    number += 1
-            elif predict >= 70 and predict <= 80:
-                if number < 70:
-                    number += 10
+                    predict += 1
+            elif number >= 70 and number <= 80:
+                if predict < 70:
+                    predict += 10
                 else:
-                    number += 1
-            elif predict >= 80 and predict <= 90:
-                if number < 80:
-                    number += 10
+                    predict += 1
+            elif number >= 80 and number <= 90:
+                if predict < 80:
+                    predict += 10
                 else:
-                    number += 1
-            elif predict >= 90 and predict <= 100:
-                if number < 90:
-                    number += 10
+                    predict += 1
+            elif number >= 90 and number <= 100:
+                if predict < 90:
+                    predict += 10
                 else:
-                    number += 1
+                    predict += 1
 
         else:
-            if predict < 10:
-                if number > 10:
-                    number -= 10
+            if number < 10:
+                if predict > 10:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 90 and predict <= 100:
-                if number > 100:
-                    number -= 10
+                    predict -= 1
+            elif number >= 90 and number <= 100:
+                if predict > 100:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 80 and predict <= 90:
-                if number > 90:
-                    number -= 10
+                    predict -= 1
+            elif number >= 80 and number <= 90:
+                if predict > 90:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 70 and predict <= 80:
-                if number > 90:
-                    number -= 10
+                    predict -= 1
+            elif number >= 70 and number <= 80:
+                if predict > 90:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 60 and predict <= 70:
-                if number > 70:
-                    number -= 10
+                    predict -= 1
+            elif number >= 60 and number <= 70:
+                if predict > 70:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 50 and predict <= 60:
-                if number > 60:
-                    number -= 10
+                    predict -= 1
+            elif number >= 50 and number <= 60:
+                if predict > 60:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 40 and predict <= 50:
-                if number > 50:
-                    number -= 10
+                    predict -= 1
+            elif number >= 40 and number <= 50:
+                if predict > 50:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 30 and predict <= 40:
-                if number > 40:
-                    number -= 10
+                    predict -= 1
+            elif number >= 30 and number <= 40:
+                if predict > 40:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 20 and predict <= 30:
-                if number > 30:
-                    number -= 10
+                    predict -= 1
+            elif number >= 20 and number <= 30:
+                if predict > 30:
+                    predict -= 10
                 else:
-                    number -= 1
-            elif predict >= 10 and predict <= 20:
-                if number > 20:
-                    number -= 10
+                    predict -= 1
+            elif number >= 10 and number <= 20:
+                if predict > 20:
+                    predict -= 10
                 else:
-                    number -= 1
+                    predict -= 1
 
     # Ваш код заканчивается здесь
 
